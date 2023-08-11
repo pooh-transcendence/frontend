1b2e41559e37a638e6e82cc05854c0014fcccccb
kylew1004 <kylew1004@gmail.com>
2023-08-11 16:43:20 +0900

    [FIX] resolve fonts

--- src/app/api.ts
+++ src/app/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // export const auth="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwibmlja25hbWUiOiJoZWxsbzIiLCJmdElkIjoiam9vd3Bhcmsyc3RkdWVudC5zZW91bC5rciIsImlhdCI6MTY5MTQ2MDE3MiwiZXhwIjoxNjk0MDUyMTcyfQ.mAWRJ_UMpeDQ1zHPX5V4K3q2E63-OuptuViqWeIcEuA";
 // export const baseUrl="http://localhost:3000";
-export const auth="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwibmlja25hbWUiOiJqb293cGFyayIsImZ0SWQiOiJqb293cGFyayIsImlhdCI6MTY5MTczMTA4OCwiZXhwIjoxNjk0MzIzMDg4fQ.F2R2Qf02IqY9pwvUzKQVoj8GayO2N9f3l9K3t4qt4DM";
+export const auth="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mywibmlja25hbWUiOiJrbGV3IiwiZnRJZCI6ImtsZXciLCJpYXQiOjE2OTE3MzcwNTgsImV4cCI6MTY5NDMyOTA1OH0.Fh7i1fLvKjPbHpjS0sH80nYLq_InPidkqOd1SC-iYoQ";
 export const baseUrl="http://10.19.233.166:8484";
 
 export let socket = io(baseUrl+"/channel",
