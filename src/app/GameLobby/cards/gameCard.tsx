58a0773ef5a742ffeef1d8f9492d220dd08643ad
r4pidstart <r4pidstart@gmail.com>
2023-09-02 16:40:07 +0900

    fixed gameCard layout

--- src/app/GameLobby/cards/gameCard.tsx
+++ src/app/GameLobby/cards/gameCard.tsx
@@ -40,14 +40,12 @@ export default function GameCard({ game }: { game: GameInfo }): JSX.Element {
     <>
       <div className="relative w-[43.56rem] h-[2.56rem]">
         <img
-          className="absolute top-[2.31rem] left-[2.69rem] w-[40.88rem] h-[0.13rem]"
-          alt=""
+          className="relative top-[2.31rem] left-[2.69rem] w-[40.88rem] h-[0.13rem]"
           src="/line-2.svg"
         />
         <img
-          className="absolute h-[78.05%] w-[4.59%] top-[0%] right-[95.41%] bottom-[21.95%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
-          alt=""
-          src="/pngegg-4@2x.png"
+          className="relative rounded-[70%] h-[78.05%] w-[4.59%] top-[0%] right-[95.41%] bottom-[21.95%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+          src={opponent?.avatar ? opponent.avatar : "/pngegg-4@2x.png"}
         />
         <div className="absolute top-[0.94rem] left-[30.94rem] inline-block w-[9.31rem] h-[1.31rem]">
           [game settings(todo) {game.ballSpeed} {game.racketSize}]
