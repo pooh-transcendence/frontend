eb86be6242b09a9f9013f4515c63ce7aa898a64f
r4pidstart <r4pidstart@gmail.com>
2023-09-04 16:50:36 +0900

    show match without one that created by me

--- src/app/GameLobby/frame/GameLobbyFrame.tsx
+++ src/app/GameLobby/frame/GameLobbyFrame.tsx
@@ -281,8 +281,8 @@ function MatchList() {
     <>
       <div className="matchList absolute overflow-auto scrollbar-hide h-[82%] w-[93.56%] top-[13.69%] right-[2.68%] bottom-[20.22%] left-[3.76%] flex flex-col items-start justify-start gap-[0.81rem]">
         {gameList.map((elem, idx) => {
-          // if(state.userInfo.id != elem.userId)
-          return <GameCard key={idx} game={elem} />;
+          if(state.userInfo.id != elem.userId)
+            return <GameCard key={idx} game={elem} />;
         })}
       </div>
     </>
