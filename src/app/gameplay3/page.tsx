e223d7210da4ac4aab99174f94ba6158b1eb54a7
ghonman2 <ghonman2@gmail.com>
2023-09-07 22:32:52 +0900

    [FIX] play game function init

--- src/app/gameplay3/page.tsx
+++ src/app/gameplay3/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { useContext, useRef, useState, useEffect } from "react";
-import { gameSocket, api_get } from "@/app/api";
-import { UserContext, userInfo } from "../UserContext";
+import { useContext, useRef, useState, useEffect } from 'react';
+import { gameSocket, api_get } from '@/app/api';
+import { UserContext, userInfo } from '../UserContext';
 
 interface gameInfo {
   participants: number[];
@@ -11,14 +11,14 @@ interface gameInfo {
   score: number[];
   ball: number[];
   isGetScore: boolean;
-  whoAmI: "left" | "right";
+  whoAmI: 'left' | 'right';
   nickname: string;
 }
 
 interface ball {
   new: (
     incrementedSpeed?: number,
-    paddleSize?: number
+    paddleSize?: number,
   ) => {
     width: number;
     height: number;
@@ -30,14 +30,13 @@ interface ball {
 interface ai {
   new: (
     side: string,
-    whoAmI: boolean
+    whoAmI: boolean,
   ) => {
     width: number;
     height: number;
     x: number;
     y: number;
     score: number;
-    color: string;
   };
 }
 
@@ -63,7 +62,7 @@ type game = GameObject & {
 
 interface gameResult {
   id: number;
-  gameType: "LADDER" | "1vs1 PUBLIC" | "1vs1 PRIVATE";
+  gameType: 'LADDER' | '1vs1 PUBLIC' | '1vs1 PRIVATE';
   winner: userInfo;
   loser: userInfo;
   winScore: number;
@@ -76,7 +75,7 @@ function ReadyForm() {
 
   const readyHandler = () => {
     setReady(true);
-    gameSocket.emit("gameStart");
+    gameSocket.emit('gameStart');
   };
 
   return (
@@ -110,12 +109,12 @@ function ReadyForm() {
         #9747FF 30%,
         #555555 50%
       )`,
-          WebkitBackgroundClip: "text",
-          backgroundClip: "text",
-          WebkitTextFillColor: "transparent",
-          textFillColor: "transparent",
-          backgroundSize: "500% auto",
-          animation: "textShine 1s ease-in-out infinite alternate",
+          WebkitBackgroundClip: 'text',
+          backgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          textFillColor: 'transparent',
+          backgroundSize: '500% auto',
+          animation: 'textShine 1s ease-in-out infinite alternate',
         }}
       >
         wait for ready...
@@ -135,8 +134,8 @@ function GameEnd({ game }: { game: gameResult }) {
           <button
             onClick={() => {
               actions.setShowGame(false);
-              api_get("/user").then((res) => {
-                console.log("/user", res);
+              api_get('/user').then((res) => {
+                console.log('/user', res);
                 const data: userInfo = res.data.data;
                 actions.setUserInfo({
                   ...state.userInfo,
@@ -154,14 +153,14 @@ function GameEnd({ game }: { game: gameResult }) {
           {game.winner.id == state.userInfo.id ? (
             <div className="YouWin left-[191px] top-[21px] absolute">
               <span className="text-neutral-600 text-[32px] font-bold">
-                you{" "}
+                you{' '}
               </span>
               <span className="text-blue-900 text-[32px] font-bold">win!</span>
             </div>
           ) : (
             <div className="YouLose left-[188px] top-[21px] absolute">
               <span className="text-neutral-600 text-[32px] font-bold">
-                you{" "}
+                you{' '}
               </span>
               <span className="text-pink-800 text-[32px] font-bold">lose!</span>
             </div>
@@ -179,14 +178,14 @@ function GameEnd({ game }: { game: gameResult }) {
               <img
                 className="Pngegg2 w-7 h-7"
                 src={
-                  game.winner.avatar ? game.winner.avatar : "/pngegg-4@2x.png"
+                  game.winner.avatar ? game.winner.avatar : '/pngegg-4@2x.png'
                 }
               />
             </div>
             <div className="P2 justify-start items-start absolute left-[18.84rem] top-[4.69rem] gap-[8px] flex">
               <img
                 className="Pngegg2 w-7 h-7"
-                src={game.loser.avatar ? game.loser.avatar : "/pngegg-4@2x.png"}
+                src={game.loser.avatar ? game.loser.avatar : '/pngegg-4@2x.png'}
               />
               <div className="Yourname text-left text-neutral-600 text-[24px] font-bold">
                 {game.loser.nickname}
@@ -220,26 +219,26 @@ function GamePlayRoomPages() {
     const keyDownHandler = (key: KeyboardEvent) => {
       // Handle up arrow and w key events
       if (key.keyCode === 38 || key.keyCode === 87)
-        gameSocket.emit("updateRacket", {
+        gameSocket.emit('updateRacket', {
           userId: state.userInfo.id,
           direction: 1,
         });
 
       // Handle down arrow and s key events
       if (key.keyCode === 40 || key.keyCode === 83)
-        gameSocket.emit("updateRacket", {
+        gameSocket.emit('updateRacket', {
           userId: state.userInfo.id,
           direction: -1,
         });
     };
 
     const joinQueueListener = (data: any) => {
-      console.log("joinQueue", data);
+      console.log('joinQueue', data);
     };
 
     const gameEndListener = (data: any) => {
       setGameEnd(data);
-      console.log("gameEnd", data);
+      console.log('gameEnd', data);
     };
 
     const gameGiveupEndListener = (data: any) => {
@@ -248,29 +247,29 @@ function GamePlayRoomPages() {
 
     const gameStartHandler = (data: gameInfo) => {
       setGameInfo(data);
-      console.log("gameStartHandler", data);
+      console.log('gameStartHandler', data);
       console.log(data);
       Pong.initialize(data);
-      document.addEventListener("keydown", keyDownHandler);
-      gameSocket.emit("gameStart");
+      document.addEventListener('keydown', keyDownHandler);
+      gameSocket.emit('gameStart');
     };
 
-    gameSocket.on("joinQueue", joinQueueListener); // only for random match
-    gameSocket.on("gameUpdate", gameUpdateListener);
-    gameSocket.on("gameEnd", gameEndListener);
-    gameSocket.on("gameStart", gameStartHandler); // detects game is started
+    gameSocket.on('joinQueue', joinQueueListener); // only for random match
+    gameSocket.on('gameUpdate', gameUpdateListener);
+    gameSocket.on('gameEnd', gameEndListener);
+    gameSocket.on('gameStart', gameStartHandler); // detects game is started
 
     return () => {
-      gameSocket.off("joinQueue", joinQueueListener);
-      gameSocket.off("gameUpdate", gameUpdateListener);
-      gameSocket.off("gameEnd", gameEndListener);
-      gameSocket.off("gameStart", gameStartHandler);
-      document.removeEventListener("keydown", keyDownHandler);
+      gameSocket.off('joinQueue', joinQueueListener);
+      gameSocket.off('gameUpdate', gameUpdateListener);
+      gameSocket.off('gameEnd', gameEndListener);
+      gameSocket.off('gameStart', gameStartHandler);
+      document.removeEventListener('keydown', keyDownHandler);
     };
   }, []);
 
   const rounds = [5];
-  const colors = ["#1abc9c", "#2ecc71", "#3498db", "#8c52ff", "#9b59b6"];
+  const colors = ['#1abc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6'];
 
   let Ball: ball = {
     new: function (incrementedSpeed?: number) {
@@ -287,37 +286,35 @@ function GamePlayRoomPages() {
   let Ai: ai = {
     new: function (side: string, gameReadyDto?: any) {
       // if (canvasRef.current === null) return null;
-
       return {
         width: 18,
         height: gameReadyDto.paddleSize,
-        x: side === "left" ? 150 : canvasRef.current!.width - 150,
+        x: side === 'left' ? 150 : canvasRef.current!.width - 150,
         y: canvasRef.current!.height / 2 - 35,
         score: 0,
-        color: gameReadyDto.whoAmI === "left" ? "#3498db" : "#e74c3c",
       };
     },
   };
 
   const Game: any = {
     initialize: function (this: game, gameReadyDto: any) {
-      console.log("initialize");
+      console.log('initialize');
       this.canvas = canvasRef;
       if (this.canvas.current === null) return null;
-      this.context = this.canvas.current.getContext("2d");
+      this.context = this.canvas.current.getContext('2d');
 
       this.canvas.width = 1400;
       this.canvas.height = 1000;
 
       this.player = new Array(2);
       if (gameReadyDto) {
-        this.player[0] = Ai.new.call(this, "left", gameReadyDto);
-        this.player[1] = Ai.new.call(this, "right", gameReadyDto);
+        this.player[0] = Ai.new.call(this, 'left', gameReadyDto);
+        this.player[1] = Ai.new.call(this, 'right', gameReadyDto);
         this.ball = Ball.new.call(this, gameReadyDto.racketSize);
         this.whoAmI = gameReadyDto.whoAmI;
       } else {
-        this.player[0] = Ai.new.call(this, "left", true);
-        this.player[1] = Ai.new.call(this, "right", false);
+        this.player[0] = Ai.new.call(this, 'left', true);
+        this.player[1] = Ai.new.call(this, 'right', false);
         this.ball = Ball.new.call(this);
       }
       // this.ai.speed = 5;
@@ -325,10 +322,10 @@ function GamePlayRoomPages() {
 
       // this.turn = this.ai;
       this.timer = this.round = 0;
-      this.color = "#8c52ff00"; // background color
-
-      Pong.draw();
+      this.color = '#8c52ff00'; // background color
+      if (gameReadyDto) Pong.draw();
     },
+
     // Draw the objects to the canvas element
     draw: function () {
       // Clear the Canvas
@@ -341,7 +338,7 @@ function GamePlayRoomPages() {
       this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
       // Set the fill style to white (For the paddles and the ball)
-      this.context.fillStyle = "#9747ff";
+      this.context.fillStyle = '#9747ff';
 
       this.player.forEach((player: any) => {
         this.context.fillRect(player.x, player.y, player.width, player.height);
@@ -353,33 +350,41 @@ function GamePlayRoomPages() {
       this.context.lineTo(this.canvas.width / 2, 140);
       this.context.lineWidth = 10;
 
-      this.context.strokeStyle = "#9747ff";
+      this.context.strokeStyle = '#9747ff';
       this.context.stroke();
 
       // Set the default canvas font and align it to the center
-      this.context.font = "100px Inria Sans";
-      this.context.textAlign = "center";
+      this.context.font = '100px Inria Sans';
+      this.context.textAlign = 'center';
 
       this.player.forEach((player: any, index: number) => {
         const place = index === 0 ? -300 : 300;
         this.context.fillText(
           player.score.toString(),
           this.canvas.width / 2 + index,
-          200
+          200,
         );
       });
+
       // Change the font size for the center score text
-      this.context.font = "100px Inria Sans";
+      this.context.font = '100px Inria Sans';
 
       // Change the font size for the center score value
-      this.context.font = "40px Inria Sans";
+      this.context.font = '40px Inria Sans';
 
       // Draw the current round number
       this.context.fillText(
         rounds[Pong.round] ? rounds[Pong.round] : rounds[Pong.round - 1],
         this.canvas.width / 2,
-        100
+        100,
       );
+      if (this.whoAmI) {
+        this.context.fillText(
+          'Where Am I ? : ' + this.whoAmI, //"Round " + (Pong.round + 1),
+          this.canvas.width / 2,
+          35,
+        );
+      }
     },
 
     drawData: function (data: gameInfo, whoAmI: string, isGameeady: boolean) {
@@ -393,7 +398,7 @@ function GamePlayRoomPages() {
       this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
       // Set the fill style to white (For the paddles and the ball)
-      this.context.fillStyle = "#9747ff";
+      this.context.fillStyle = '#9747ff';
 
       // Draw the Player
       this.context.fillRect(
@@ -401,7 +406,7 @@ function GamePlayRoomPages() {
         //this.player.y,
         data.racket[0][1], //this.player.width,
         this.player[0].width,
-        this.player[0].height
+        this.player[0].height,
       );
 
       // Draw the Counter Player
@@ -409,7 +414,7 @@ function GamePlayRoomPages() {
         data.racket[1][0],
         data.racket[1][1],
         this.player[1].width,
-        this.player[1].height
+        this.player[1].height,
       );
       // Draw the Ball
       //if (Pong._turnDelayIsOver.call(this))
@@ -418,7 +423,7 @@ function GamePlayRoomPages() {
         data.ball[1], //| this.ball.y,
         //this.ball.y,
         this.ball.width,
-        this.ball.height
+        this.ball.height,
       );
       // Draw the net (Line in the middle)
       this.context.beginPath();
@@ -426,41 +431,41 @@ function GamePlayRoomPages() {
       this.context.moveTo(this.canvas.width / 2, this.canvas.height - 140);
       this.context.lineTo(this.canvas.width / 2, 140);
       this.context.lineWidth = 10;
-      this.context.strokeStyle = "#9747ff";
+      this.context.strokeStyle = '#9747ff';
 
       this.context.stroke();
 
       // Set the default canvas font and align it to the center
-      this.context.font = "100px Inria Sans";
-      this.context.textAlign = "center";
+      this.context.font = '100px Inria Sans';
+      this.context.textAlign = 'center';
 
       // Draw the players score (left)
       this.context.fillText(
         data.score[0].toString(),
         //this.player.score.toString(),
         this.canvas.width / 2 - 300,
-        200
+        200,
       );
 
       // Draw the paddles score (right)
       this.context.fillText(
         data.score[1].toString(),
         this.canvas.width / 2 + 300,
-        200
+        200,
       );
 
       // Change the font size for the center score text
-      this.context.font = "30px Inria Sans";
+      this.context.font = '30px Inria Sans';
 
       // Draw the winning score (center)
       this.context.fillText(
-        "Where Am I ? : " + this.whoAmI, //"Round " + (Pong.round + 1),
+        'Where Am I ? : ' + this.whoAmI, //"Round " + (Pong.round + 1),
         this.canvas.width / 2,
-        35
+        35,
       );
 
       // Change the font size for the center score value
-      this.context.font = "40px Inria Sans";
+      this.context.font = '40px Inria Sans';
     },
   };
 
